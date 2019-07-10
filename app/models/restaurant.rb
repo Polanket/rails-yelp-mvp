5de384cc486c9d34e67c404f4acdefdf48a747a6
Juan Polanco <juanmpt@outlook.com>
2019-07-10 12:25:20 +0200

    created restaurant_controller

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
   validates :name, presence: true
   validates :address, presence: true
   validates :category, presence: true
