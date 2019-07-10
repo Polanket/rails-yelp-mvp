13feb4e5d38b56731adc33c0bdceefe4fa5691b7
Juan Polanco <juanmpt@outlook.com>
2019-07-10 12:14:29 +0200

    created Review model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,10 @@
+# frozen_string_literal: true
+
 class Restaurant < ApplicationRecord
   validates :name, presence: true
   validates :address, presence: true
   validates :category, presence: true
-  validates :category, inclusion: { in: ['chinese', 'italian', 'japanese', 'french', 'belgian'] }
+  validates :category, inclusion: {
+    in: %w[chinese italian japanese french belgian]
+  }
 end
