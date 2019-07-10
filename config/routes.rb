14a052839692d8ac0a3e4182cede7cf235b4201c
Juan Polanco <juanmpt@outlook.com>
2019-07-10 13:07:00 +0200

    Created the review controller with new and create methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :restaurants
+  resources :restaurants, except: %I[edit update destroy] do
+    resources :reviews, only: %I[new create]
+  end
 end
